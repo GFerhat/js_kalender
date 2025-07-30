732334f37c88c12ee96898924df33230b515dfc3
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-07-30 17:36:07 +0200

    Update calendar styles and enhance date display functionality

--- index.js
+++ index.js
@@ -3,45 +3,53 @@ console.log(datumAktuell);
 let datum = datumAktuell.getDate();
 console.log(datum);
 let monat = datumAktuell.getMonth() +1;
-let monatText;
+let monatName;
+ 
+const wochentagArray = [" Sonntag "," Montag "," Dienstag "," Mittwoch "," Donnerstag "," Freitag "," Samstag "];
+let wochentagNum = datumAktuell.getDay();
+let wochentag = wochentagArray[wochentagNum];
+console.log(wochentag);
+
+function monatWochenRechner {
+  wochentagArray.getDay
+}
 
 let monatNummer
 if (monat == 1) {
-  monatText = 'Januar';
+  monatName = 'Januar';
 } else if (monat == 2) {
-  monatText = 'Februar';
+  monatName = 'Februar';
 } else if (monat == 3) {
-  monatText = 'März';
+  monatName = 'März';
 } else if (monat == 4) {
-  monatText = 'April';
+  monatName = 'April';
 } else if (monat == 5) {
-  monatText = 'Mai';
+  monatName = 'Mai';
 } else if (monat == 6) {
-  monatText = 'Juni';
+  monatName = 'Juni';
 } else if (monat == 7) {
-  monatText = 'Juli';
+  monatName = 'Juli';
 } else if (monat == 8) {
-  monatText = 'August';
+  monatName = 'August';
 } else if (monat == 9) {
-  monatText = 'September';
+  monatName = 'September';
 } else if (monat == 10) {
-  monatText = 'Oktober';
+  monatName = 'Oktober';
 } else if (monat == 11) {
-  monatText = 'November';
+  monatName = 'November';
 } else {
-  monatText = 'Dezember';
+  monatName = 'Dezember';
 }
 
-console.log('Monat ausgeschrieben: ' +monatText)
-
+console.log('Monat ausgeschrieben: ' + monatName)
 if (monat < 10) {
   monat = "0" + (monat);
 }
 console.log(monat);
 let jahr = datumAktuell.getFullYear();
 console.log(jahr);
-let monatwoche;
 
+let monatwoche;
 if (datum < 7) {
   monatwoche = 1;
 } else if (datum < 14) {
@@ -54,10 +62,16 @@ if (datum < 7) {
   monatwoche = 5;
 }
 
-function datumGanzAusgeben() { //Funktion erstellt das heutige Datum
-  return datum + '.' + monat + "." + jahr;
+function vollesDatum() { //Funktion erstellt das heutige Datum
+  return datum + '.' + monat + "." + jahr + " ";
   
 }
-console.log(datumGanzAusgeben());
+console.log(vollesDatum());
 
-console.log('Die ' + monatwoche + ' Woche des Monats ' + monatText);
+console.log('Die ' + monatwoche + ' Woche des Monats ' + monatName);
+
+document.getElementById("datumHistorie").innerHTML = monatName;
+document.getElementById("datumInfo").innerHTML = vollesDatum();
+document.getElementById("wochentagInfo1").innerHTML = wochentag;
+document.getElementById("wochentagInfo2").innerHTML = wochentag;
+document.getElementById("zahlWochentag").innerHTML = monatwoche + "te";
