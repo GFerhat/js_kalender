27a53a48092d13f66ee46780c54e047c3928a993
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-07-31 14:37:42 +0200

    Refactor date handling and update HTML elements for improved date display

--- index.js
+++ index.js
@@ -1,77 +1,69 @@
 let datumAktuell = new Date();
 console.log(datumAktuell);
-let datum = datumAktuell.getDate();
-console.log(datum);
-let monat = datumAktuell.getMonth() +1;
-let monatName;
- 
-const wochentagArray = [" Sonntag "," Montag "," Dienstag "," Mittwoch "," Donnerstag "," Freitag "," Samstag "];
+let datumTag = datumAktuell.getDate();
+console.log("aktueller Tag " + datumTag);
+let monat = datumAktuell.getMonth();
+
+
+const wochentagArray = ["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"];
 let wochentagNum = datumAktuell.getDay();
+console.log("Wochentagindex " + wochentagNum);
 let wochentag = wochentagArray[wochentagNum];
-console.log(wochentag);
+console.log("Wochentag: " + wochentag);
 
-function monatWochenRechner {
-  wochentagArray.getDay
+
+function tageBisMontagCalc(wochentagNum) { //Rechnet den aktuellen Tag -1 bis Montag
+  let tageBisMontag=0;
+  while (wochentagNum !== 1 ) { 
+  tageBisMontag++;
+  wochentagNum--;
+  if (wochentagNum < 0) {
+      wochentagNum=6;
+    }
+  }
+return tageBisMontag;
 }
+let tageBisMontag = tageBisMontagCalc(wochentagNum);
+tageBisMontagCalc(tageBisMontag);
+console.log(tageBisMontag);
 
-let monatNummer
-if (monat == 1) {
-  monatName = 'Januar';
-} else if (monat == 2) {
-  monatName = 'Februar';
-} else if (monat == 3) {
-  monatName = 'März';
-} else if (monat == 4) {
-  monatName = 'April';
-} else if (monat == 5) {
-  monatName = 'Mai';
-} else if (monat == 6) {
-  monatName = 'Juni';
-} else if (monat == 7) {
-  monatName = 'Juli';
-} else if (monat == 8) {
-  monatName = 'August';
-} else if (monat == 9) {
-  monatName = 'September';
-} else if (monat == 10) {
-  monatName = 'Oktober';
-} else if (monat == 11) {
-  monatName = 'November';
-} else {
-  monatName = 'Dezember';
-}
+const monatArray = ["Januar", "Februar", "März", "April","Mai","Juni", 
+  "Juli", "August", "September", "Oktober", "November", "Dezember"]
+let monatNum=datumAktuell.getMonth();
+console.log("Monatindex: " + monatNum);
+let monatName = monatArray[monatNum];
+console.log("Name des Monats:" + monatName)
 
-console.log('Monat ausgeschrieben: ' + monatName)
 if (monat < 10) {
-  monat = "0" + (monat);
+  monat = "0" + (monat+1);
 }
 console.log(monat);
 let jahr = datumAktuell.getFullYear();
 console.log(jahr);
 
 let monatwoche;
-if (datum < 7) {
+if (datumTag < 7) {
   monatwoche = 1;
-} else if (datum < 14) {
+} else if (datumTag < 14) {
   monatwoche = 2;
-} else if (datum < 21) {
+} else if (datumTag < 21) {
   monatwoche = 3; 
-} else if (datum < 28) {
+} else if (datumTag < 28) {
   monatwoche = 4;
 } else {
   monatwoche = 5;
 }
 
-function vollesDatum() { //Funktion erstellt das heutige Datum
-  return datum + '.' + monat + "." + jahr + " ";
-  
+function vollesDatum() { //Funktion erstellt das heutige datumTag
+  return datumTag + '.' + monat  + "." + jahr + " ";
 }
-console.log(vollesDatum());
 
+console.log(vollesDatum());
 console.log('Die ' + monatwoche + ' Woche des Monats ' + monatName);
 
 document.getElementById("datumHistorie").innerHTML = monatName;
 document.getElementById("datumInfo").innerHTML = vollesDatum();
+document.getElementById("datumInfo1").innerHTML = vollesDatum();
 document.getElementById("wochentagInfo1").innerHTML = wochentag;
 document.getElementById("wochentagInfo2").innerHTML = wochentag;
 document.getElementById("zahlWochentag").innerHTML = monatwoche + "te";
